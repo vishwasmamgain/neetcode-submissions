6118acf94a7c0300f8dc2c4e651fb2f5c48b4039
Vishwas Mamgain <158207720+vishwasmamgain@users.noreply.github.com>
2026-07-23 02:00:20 +0530

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length()!=t.length()){
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+         for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)){
+                return false;
+            }
+
+            map.put(c,map.get(c)-1);
+
+            if(map.get(c)==0){
+                map.remove(c);
+            }
+
+        }
+        return map.isEmpty();
+    }
+}
